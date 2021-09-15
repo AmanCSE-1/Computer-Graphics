4dc44ef73fb6575fe0f5f25fd5cdac1d25899d33
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-09-15 22:54:55 +0530

    Added Function for 4-connected approach
    The function is added which fill the area by using Flood-Fill Algorithm by using 4-connected approach

--- Area-Filling-Algorithm/Flood-Fill Algorithm.c
+++ Area-Filling-Algorithm/Flood-Fill Algorithm.c
@@ -2,3 +2,28 @@
 #include<graphics.h>
 #include<dos.h>
 #include<conio.h>
+
+void floodFill4(int x, int y, int newColor, int oldColor){
+    if(getpixel(x,y) == oldColor){
+        delay(5);
+        putpixel(x, y, newColor);
+        floodFill4(x+1, y, newColor, oldColor);
+        floodFill4(x, y+1, newColor, oldColor);
+        floodFill4(x-1, y, newColor, oldColor);
+        floodFill4(x, y-1, newColor, oldColor);
+    }
+}
+
+int main(){
+    int gDriver=DETECT, gMode, xc, yc, radius;
+    initgraph(&gDriver, &gMode, "C:\\TURBOC3\\BGI");
+    
+    printf("\nFlood Fill Algorithm");
+    printf("\n\nFirst Square filled by 4 connected approach");
+    rectangle(150, 250, 200, 300);
+    floodFill4(175, 275, 7, 0);
+  
+    closegraph();
+    return 0;
+}
+
