c308d94fdffaace202d8436dfd385263a6bd5de7
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-09-15 22:59:44 +0530

    Fixed Indentation for the program

--- Area-Filling-Algorithm/Boundary Fill Algorithm.c
+++ Area-Filling-Algorithm/Boundary Fill Algorithm.c
@@ -5,46 +5,46 @@
 
 
 void boundaryFill4(int x, int y, int fillColor, int bColor){
-  if(getpixel(x,y)!=bColor && getpixel(x,y)!=fillColor){
-    delay(5);
-    putpixel(x, y, fillColor);
-    boundaryFill4(x+1, y, fillColor, bColor);
-    boundaryFill4(x, y+1, fillColor, bColor);
-    boundaryFill4(x-1, y, fillColor, bColor);
-    boundaryFill4(x, y-1, fillColor, bColor);
-  }
+    if(getpixel(x,y)!=bColor && getpixel(x,y)!=fillColor){
+        delay(5);
+        putpixel(x, y, fillColor);
+        boundaryFill4(x+1, y, fillColor, bColor);
+        boundaryFill4(x, y+1, fillColor, bColor);
+        boundaryFill4(x-1, y, fillColor, bColor);
+        boundaryFill4(x, y-1, fillColor, bColor);
+    }
 }
 
 void boundaryFill8(int x, int y, int fillColor, int bColor){
-  if(getpixel(x,y)!=bColor && getpixel(x,y)!=fillColor){
-    delay(5);
-    putpixel(x, y, fillColor);
-    boundaryFill8(x+1, y-1, fillColor, bColor);
-    boundaryFill8(x+1, y, fillColor, bColor);
-    boundaryFill8(x+1, y+1, fillColor, bColor);
-    boundaryFill8(x, y+1, fillColor, bColor);
-    boundaryFill8(x-1, y+1, fillColor, bColor);
-    boundaryFill8(x-1, y, fillColor, bColor);
-    boundaryFill8(x-1, y-1, fillColor, bColor);
-    boundaryFill8(x, y-1, fillColor, bColor);
-  }
+    if(getpixel(x,y)!=bColor && getpixel(x,y)!=fillColor){
+        delay(5);
+        putpixel(x, y, fillColor);
+        boundaryFill8(x+1, y-1, fillColor, bColor);
+        boundaryFill8(x+1, y, fillColor, bColor);
+        boundaryFill8(x+1, y+1, fillColor, bColor);
+        boundaryFill8(x, y+1, fillColor, bColor);
+        boundaryFill8(x-1, y+1, fillColor, bColor);
+        boundaryFill8(x-1, y, fillColor, bColor);
+        boundaryFill8(x-1, y-1, fillColor, bColor);
+        boundaryFill8(x, y-1, fillColor, bColor);
+    }
 }
 
 
 int main(){
-  int gDriver=DETECT, gMode, xc, yc, radius;
-  initgraph(&gDriver, &gMode, "C:\\TURBOC3\\BGI");
-  
-  printf("\nBoundary Fill Algorithm");
-  
-  printf("\n\nFirst Square filled by 4 connected approach");
-  rectangle(100, 150, 150, 200);
-  boundaryFill4(125, 175, 7, 15);
-  
-  printf("\n\nNext Square filled by 8 connected approach");
-  rectangle(250, 150, 300, 200);
-  boundaryFill8(275, 175, 9, 15);
-  
-  closegraph();
-  return 0;
+    int gDriver=DETECT, gMode, xc, yc, radius;
+    initgraph(&gDriver, &gMode, "C:\\TURBOC3\\BGI");
+
+    printf("\nBoundary Fill Algorithm");
+
+    printf("\n\nFirst Square filled by 4 connected approach");
+    rectangle(100, 150, 150, 200);
+    boundaryFill4(125, 175, 7, 15);
+
+    printf("\n\nNext Square filled by 8 connected approach");
+    rectangle(250, 150, 300, 200);
+    boundaryFill8(275, 175, 9, 15);
+
+    closegraph();
+    return 0;
 }
