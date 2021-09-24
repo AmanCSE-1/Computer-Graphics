5dc1336632f1dc869c22962b96f3b0d10c1c16e5
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-09-24 23:44:50 +0530

    Added comments for enhancing interpretability

--- Transformation on 2D objects/Shearing Transformation.c
+++ Transformation on 2D objects/Shearing Transformation.c
@@ -10,22 +10,22 @@ int main(){
     initgraph(&gDriver, &gMode, "C:\\TURBOC3\\BGI");
 
     printf("2D Shearing Transformation\n");
-    x1=100; y1=100;
+    x1=100; y1=100;                                 // (x1,y1), (x2,y2), (x3,y3) are vertices of Traingle
     x2=100; y2=150;
     x3=150; y3=150;
 
-    line(x1, y1, x2, y2);
+    line(x1, y1, x2, y2);                           // Drawing triangle by using lines
     line(x2, y2, x3, y3);
     line(x1, y1, x3, y3);
 
-    printf("\nEnter value of angle: ");
+    printf("\nEnter value of angle: ");             // Taking User-input for angle 
     scanf("%d", &angle);
 
-    x1+=floor(y1*tan(angle*3.14/180));
-    x2+=floor(y2*tan(angle*3.14/180));
-    x3+=floor(y3*tan(angle*3.14/180));
+    x1+=floor(y1*tan(angle*3.14/180));              // Computing the coordinates of vertices after shearing transformation 
+    // x2+=floor(y2*tan(angle*3.14/180));
+    // x3+=floor(y3*tan(angle*3.14/180));
 
-    line(x1, y1, x2, y2);
+    line(x1, y1, x2, y2);                           // Drawing Traingle after Transformation  
     line(x2, y2, x3, y3);
     line(x1, y1, x3, y3);
 
